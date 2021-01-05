d5bc4f55dff0c8224e3859950819a62026a376e3
xionghao <xiong.hao@cososhipping.com>
2021-01-05 15:14:06 +0800

    update version

--- rollup/rollup.config.js
+++ rollup/rollup.config.js
@@ -1,17 +1,24 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import image from '@rollup/plugin-image';
+import svg from './rollup-svg';
 import babel from '@rollup/plugin-babel';
 import artTemplate from '../rollup-art-template/index';
+import define from 'rollup-plugin-define';
+const GitRevisionPlugin = require('git-revision-webpack-plugin');
+const gitRevisionPlugin = new GitRevisionPlugin();
 export default {
     input: 'src/js/index.js',
     plugins: [
-        image({
-            dom: true
-        }),
+        svg(),
         babel(
             { babelHelpers: 'bundled' }
         ),
+        define({
+            replacements: {
+                DPLAYER_VERSION: `"${require('../package.json').version}"`,
+                GIT_HASH: JSON.stringify(gitRevisionPlugin.version())
+            },
+        }),
         artTemplate(),
         resolve(), // so Rollup can find `ms`
         commonjs() // so Rollup can convert `ms` to an ES module
